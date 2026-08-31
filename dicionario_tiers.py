55f11391c37e5388cb4333a0ba5d9d4dcee394d8
raquelemanuella <raquelemanuella0603@gmail.com>
2026-08-31 17:28:15 -0300

    Create dicionario_tiers.py

---
+++ dicionario_tiers.py
@@ -0,0 +1,130 @@
+# dicionario_tiers.py
+
+DICIONARIO_VEICULOS = {
+    '99 Food': [
+        'aloalobahia.com', 'anotabahia.com', 'aratuon.com.br', 'atarde.com.br', 
+        'bahia.ba', 'bahiaeconomica.com.br', 'bahianoticias.com.br', 'band.com.br', 
+        'bandbahia.com.br', 'bandceara.com.br', 'bandnewsfm.band.uol.com.br', 'bnews.com.br', 
+        'cbn.globo.com', 'cbngoiania.com.br', 'congressoemfoco.com.br', 'correio24horas.com.br', 
+        'correiobraziliense.com.br', 'correiodamanha.com.br', 'diariodegoias.com.br', 
+        'diariodonordeste.verdesmares.com.br', 'educadora.ba.gov.br', 'em.com.br', 
+        'emtempo.com.br', 'g1.globo.com', 'globoplay.globo.com', 'gpsbrasilia.com.br', 
+        'itatiaia.com.br', 'jkfm.com.br', 'jornaldebrasilia.com.br', 'jornaltribuna.com.br', 
+        'jota.info', 'jovempan.com.br', 'maisgoias.com.br', 'metropoles.com', 'nossomeio.com.br', 
+        'oestadoce.com.br', 'ootimista.com.br', 'opopular.com.br', 'opovo.com.br', 
+        'otempo.com.br', 'poder360.com.br', 'portal6.com.br', 'radiosociedade.com.br', 
+        'record.r7.com', 'recordbahia.com.br', 'redeglobo.globo.com', 'revistayacht.com.br', 
+        'sagresonline.com.br', 'sbt.com.br', 'tmc.com.br', 'tribunadabahia.com.br', 
+        'tvcidadefortaleza.com.br', 'tve.ba.gov.br', 'tvjangadeiro.com.br', 'tvotimista.com.br', 
+        'tvuniao.tv.br', 'youtube.com'
+    ],
+    'CCBB Salvador': [
+        'aloalobahia.com', 'anotabahia.com', 'atarde.com.br', 'bahiaeconomica.com.br', 
+        'bahianoticias.com.br', 'bahiasocialvip.com.br', 'correio24horas.com.br', 
+        'g1.globo.com', 'programatonafama.com.br', 'revistayacht.com.br'
+    ],
+    'Clínica Sim': [
+        'alba.tv.br', 'aloalobahia.com', 'anotabahia.com', 'antena1.com.br', 'aratuon.com.br', 
+        'atarde.com.br', 'atardefm.com.br', 'badevalor.com.br', 'bahiaeconomica.com.br', 
+        'bahianoticias.com.br', 'bandbahia.com.br', 'bandceara.com.br', 'bandnewsfm.band.uol.com.br', 
+        'bnews.com.br', 'camara.leg.br', 'cbn.globo.com', 'correio24horas.com.br', 
+        'diariodepernambuco.com.br', 'diariodonordeste.verdesmares.com.br', 'educadora.ba.gov.br', 
+        'educadorafm.ba.gov.br', 'folhape.com.br', 'g1.globo.com', 'gfm.com.br', 'giroblog.com.br', 
+        'ibahia.com', 'ig.com.br', 'itapoanfm.com.br', 'jc.ne10.uol.com.br', 'jovempan.com.br', 
+        'leiaja.com', 'metro1.com.br', 'ne10.uol.com.br', 'nossomeio.com.br', 'novabrasilfm.com.br', 
+        'oestadoce.com.br', 'ootimista.com.br', 'opovo.com.br', 'piatafm.com.br', 'plusfm.com.br', 
+        'radioexcelsior.com.br', 'radiofolha.fm.br', 'radiojornal.com.br', 'radioclubepe.com.br', 
+        'radiosalvadorfm.com.br', 'radiosociedade.com.br', 'record.r7.com', 'recordbahia.com.br', 
+        'redeglobo.globo.com', 'revistayacht.com.br', 'robertajungmann.com.br', 'social1.ne10.uol.com.br', 
+        'trbn.com.br', 'tvcidadefortaleza.com.br', 'tvdiario.verdesmares.com.br', 'tve.ba.gov.br', 
+        'tvguararapes.com.br', 'tvjangadeiro.com.br', 'tvjornal.ne10.uol.com.br', 'tvotimista.com.br', 
+        'tvtribunape.com.br', 'tvuniao.tv.br', 'uol.com.br', 'urbnews.com.br', 'verdinha.verdesmares.com.br', 
+        'youtube.com', 'rádio verdinha - youtube'
+    ],
+    'Faculdade Baiana de Direito e Gestão': [
+        'aloalobahia.com', 'ambitojuridico.com.br', 'anotabahia.com', 'antena1.com.br', 'atarde.com.br', 
+        'atardefm.com.br', 'bahianoticias.com.br', 'bandnewsfm.band.uol.com.br', 'bnews.com.br', 
+        'camara.leg.br', 'cbn.globo.com', 'conjur.com.br', 'correio24horas.com.br', 'educadora.ba.gov.br', 
+        'g1.globo.com', 'gfm.com.br', 'ibahia.com', 'jota.info', 'jurinews.com.br', 'jusbrasil.com.br', 
+        'metro1.com.br', 'migalhas.com.br', 'novabrasilfm.com.br', 'olhardireto.com.br', 'promad.adv.br', 
+        'radiosociedade.com.br', 'revistayacht.com.br', 'trbn.com.br', 'tve.ba.gov.br', 'youtube.com'
+    ],
+    'Feed Experience Hub': [
+        'aloalobahia.com', 'anotabahia.com', 'atarde.com.br', 'bahiaeconomica.com.br', 'bahianoticias.com.br', 
+        'bahiasocialvip.com.br', 'correio24horas.com.br', 'g1.globo.com', 'programatonafama.com.br', 
+        'revistayacht.com.br'
+    ],
+    'Grupo Loro': [
+        'aloalobahia.com', 'anotabahia.com', 'atarde.com.br', 'bahiaeconomica.com.br', 'bahianoticias.com.br', 
+        'bahiasocialvip.com.br', 'correio24horas.com.br', 'g1.globo.com', 'muitogourmet.com.br', 
+        'programatonafama.com.br', 'revistayacht.com.br'
+    ],
+    'Grupo Raymundo da Fonte': [
+        'aloalobahia.com', 'anotabahia.com', 'aratuon.com.br', 'atarde.com.br', 'bahia.ba', 
+        'bahiaeconomica.com.br', 'bahianoticias.com.br', 'bandbahia.com.br', 'bandceara.com.br', 
+        'bandnewsfm.band.uol.com.br', 'bnews.com.br', 'cbn.globo.com', 'cbnamazonia.com.br', 
+        'christianemanoel.com.br', 'clickpb.com.br', 'correio24horas.com.br', 'diariodepernambuco.com.br', 
+        'diariodonordeste.verdesmares.com.br', 'diariodopara.com.br', 'diariodosertao.com.br', 'dol.com.br', 
+        'educadora.ba.gov.br', 'extracarajas.com.br', 'extradopara.com.br', 'folhape.com.br', 'g1.globo.com', 
+        'grupocorreio.com.br', 'ig.com.br', 'jc.ne10.uol.com.br', 'jornaldaparaiba.com.br', 'jornaltribuna.com.br', 
+        'jovempan.com.br', 'leiaja.com', 'liberalfm.com.br', 'maispb.com.br', 'movimentoeconomico.com.br', 
+        'ne10.uol.com.br', 'nossomeio.com.br', 'noticiaparaiba.com.br', 'oestadoce.com.br', 'oliberal.com', 
+        'ootimista.com.br', 'opovo.com.br', 'paraibaonline.com.br', 'pbagora.com.br', 'portalcorreio.com.br', 
+        'portalrba.com.br', 'portalviggo.com', 'radioclubepe.com.br', 'radiofolha.fm.br', 'radiojornal.com.br', 
+        'radiosociedade.com.br', 'record.r7.com', 'recordbahia.com.br', 'recordtvbelem.com.br', 'redeglobo.globo.com', 
+        'redepara.com.br', 'robertajungmann.com.br', 'social1.ne10.uol.com.br', 'tribunadabahia.com.br', 
+        'tvcidadefortaleza.com.br', 'tve.ba.gov.br', 'tvguararapes.com.br', 'tvjangadeiro.com.br', 
+        'tvjornal.ne10.uol.com.br', 'tvotimista.com.br', 'tvtribunape.com.br', 'uol.com.br'
+    ],
+    'JBS Alimentos': [
+        'cbn.globo.com', 'diariodepernambuco.com.br', 'folhape.com.br', 'giroblog.com.br', 'ig.com.br', 
+        'jc.ne10.uol.com.br', 'leiaja.com', 'ne10.uol.com.br', 'radioclubepe.com.br', 'radiofolha.fm.br', 
+        'radiojornal.com.br', 'record.r7.com', 'redeglobo.globo.com', 'robertajungmann.com.br', 
+        'social1.ne10.uol.com.br', 'tvguararapes.com.br', 'tvjornal.ne10.uol.com.br', 'tvtribunape.com.br', 'uol.com.br'
+    ],
+    'Villa Global Education': [
+        'alba.tv.br', 'aloalobahia.com', 'anotabahia.com', 'antena1.com.br', 'aratuon.com.br', 'atarde.com.br', 
+        'atardefm.com.br', 'bahianoticias.com.br', 'bandbahia.com.br', 'bandnewsfm.band.uol.com.br', 
+        'camara.leg.br', 'cbn.globo.com', 'correio24horas.com.br', 'educadora.ba.gov.br', 'g1.globo.com', 
+        'gfm.com.br', 'ibahia.com', 'metro1.com.br', 'novabrasilfm.com.br', 'recordbahia.com.br', 
+        'redeglobo.globo.com', 'revistayacht.com.br', 'tve.ba.gov.br', 'youtube.com'
+    ],
+    'XP Investimentos': [
+        'aloalobahia.com', 'anotabahia.com', 'aratuon.com.br', 'atarde.com.br', 'bahia.ba', 'bahiaeconomica.com.br', 
+        'bahianoticias.com.br', 'bandbahia.com.br', 'bandceara.com.br', 'bandnewsfm.band.uol.com.br', 'bnews.com.br', 
+        'cbn.globo.com', 'clickpb.com.br', 'correio24horas.com.br', 'diariodepernambuco.com.br', 
+        'diariodonordeste.verdesmares.com.br', 'diariodosertao.com.br', 'educadora.ba.gov.br', 'folhape.com.br', 
+        'g1.globo.com', 'gerardorebello.com.br', 'ig.com.br', 'jc.ne10.uol.com.br', 'jornaldaparaiba.com.br', 
+        'jornaltribuna.com.br', 'jovempan.com.br', 'leiaja.com', 'movimentoeconomico.com.br', 'ne10.uol.com.br', 
+        'nossomeio.com.br', 'oestadoce.com.br', 'opovo.com.br', 'portalcorreio.com.br', 'radioclubepe.com.br', 
+        'radiofolha.fm.br', 'radiojornal.com.br', 'radiosociedade.com.br', 'record.r7.com', 'recordbahia.com.br', 
+        'redeglobo.globo.com', 'revistayacht.com.br', 'robertajungmann.com.br', 'sistematambau.com.br', 
+        'social1.ne10.uol.com.br', 'tribunadabahia.com.br', 'tvcidadefortaleza.com.br', 'tvcorreio.com.br', 
+        'tve.ba.gov.br', 'tvguararapes.com.br', 'tvjangadeiro.com.br', 'tvjornal.ne10.uol.com.br', 'tvotimista.com.br', 
+        'tvtribunape.com.br', 'uol.com.br', 'rádio verdinha - youtube'
+    ],
+    'Ambev': [
+        '180graus.com', 'acidadeon.com', 'acontecendoaqui.com.br', 'acritica.com', 'adnews.com.br', 'agazeta.com.br', 
+        'agazetadoacre.com', 'atarde.com.br', 'atribuna.com.br', 'b9.com.br', 'bahianoticias.com.br', 'band.uol.com.br', 
+        'bloomberglinea.com.br', 'bol.uol.com.br', 'braziljournal.com', 'buzzfeed.com.br', 'canaltech.com.br', 
+        'capitalreset.com', 'caras.com.br', 'cartacapital.com.br', 'catracalivre.com.br', 'claudia.abril.com.br', 
+        'cnnbrasil.com.br', 'correio24horas.com.br', 'correiobraziliense.com.br', 'correiodoestado.com.br', 
+        'correioweb.com.br', 'd.emtempo.com.br', 'd24am.com', 'diariodepernambuco.com.br', 'diariodoamapa.com.br', 
+        'diariodonordeste.verdesmares.com.br', 'dm.com.br', 'emtempo.com.br', 'epoca.globo.com', 'epocanegocios.globo.com', 
+        'estadao.com.br', 'exame.com', 'extra.globo.com', 'folha.uol.com.br', 'folhabv.com.br', 'folhadelondrina.com.br', 
+        'folhape.com.br', 'forbes.com.br', 'g1.globo.com', 'gauchazh.clicrbs.com.br', 'gazetadigital.com.br', 
+        'gazetadopovo.com.br', 'ge.globo.com', 'gkpb.com.br', 'glamour.globo.com', 'glamurama.uol.com.br', 'globo.com', 
+        'globorural.globo.com', 'guiadacervejabr.com', 'guiadasemana.com.br', 'hojeemdia.com.br', 'ig.com.br', 
+        'imirante.com', 'imirante.com/oestadoma', 'infomoney.com.br', 'istoe.com.br', 'istoedinheiro.com.br', 
+        'jornaldaparaiba.com.br', 'jornaldotocantins.com.br', 'jornalpequeno.com.br', 'jornalvs.com.br', 'lance.com.br', 
+        'maquinadoesporte.com.br', 'meioemensagem.com.br', 'meon.com.br', 'metropoles.com', 'midianews.com.br', 'msn.com', 
+        'ne10.uol.com.br', 'neofeed.com.br', 'nsctotal.com.br', 'odia.com', 'odia.ig.com.br', 'oglobo.globo.com', 
+        'oimparcial.com.br', 'olhardireto.com.br', 'oliberal.com', 'opopular.com.br', 'opovo.com.br', 'otempo.com.br', 
+        'piauihoje.com', 'pipelinevalor.globo.com', 'poder360.com.br', 'propmark.com.br', 'r7.com', 'reuters.com', 
+        'reuters.com/world/americas', 'revistamarieclaire.globo.com', 'revistapegn.globo.com', 'rondoniaovivo.com', 
+        'sbt.com.br', 'super.abril.com.br', 'techtudo.com.br', 'tecmundo.com.br', 'terra.com.br', 'tnonline.uol.com.br', 
+        'tribunademinas.com.br', 'tribunadoceara.com.br', 'tribunadonorte.com.br', 'tribunahoje.com', 'uol.com.br', 
+        'valor.globo.com', 'veja.abril.com.br', 'vejario.abril.com.br', 'vejasp.abril.com.br', 'virgula.com.br', 
+        'vocesa.abril.com.br'
+    ]
+}
